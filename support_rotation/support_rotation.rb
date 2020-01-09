425a3db84762354f51350cffb5c2680da865c367
bianca.mouta <bianca.mouta@resultadosdigitais.com.br>
2020-01-09 13:31:17 -0300

    final use case 4

--- support_rotation/support_rotation.rb
+++ support_rotation/support_rotation.rb
@@ -1,12 +1,25 @@
 class SupportRotation 
   def initialize(teams_and_members, weeks = nil)
     @teams_and_members = teams_and_members
-    @weeks = weeks
+    @weeks = weeks    
   end
 
   def show
     @teams_and_members
   end
+
+  def count_teams 
+    @teams_and_members.length 
+  end
+
+  def count_members
+    @qtd_members = []
+    for i in (0..(count_teams - 1))
+      @qtd_members[i] = @teams_and_members[i][:members].length
+    end
+    @qtd_members
+  end
+  
   # @teams_and_members = [
   #   {team: 'email', members: ['yuri', 'rogerio', 'gabriel']},
   #   {team: 'cdp', members: ['jean g', 'jean m', 'vinicius']}
@@ -19,23 +32,28 @@ class SupportRotation
 
   def show_schedule
     schedule = {}
-    (1..2).each do |i|
+    (0..(count_teams - 1)).each do |i|
       require 'pry'
-      binding.pry
-      schedule[@teams_and_members[i-1][:team].to_sym] = {
-        "#{i}" => @teams_and_members[i-1][:members][i-1], 
-        "#{i+1}" => @teams_and_members[i-1][:members][i]
+      binding.pry        
+      schedule[@teams_and_members[i][:team].to_sym] = {
+        "#{1}" => @teams_and_members[i][:members][0]
       }
       binding.pry
+      (2..@weeks).each do |j|
+        binding.pry
+        schedule[@teams_and_members[i][:team].to_sym].merge!({
+          "#{j}" => @teams_and_members[i][:members][j-1]
+        })
+        binding.pry
+      end
     end
     schedule
   end
 end
-
 # schedule {
 #   time [0] => { week 1 => membro[0] , week 2 => membro[1]} ,
 #   time [1] => { week 1 => membro[0] , week 2 => membro[1]}
 # }
 
 #  time [0] => { week 1 => membro[0], week 2 =>membro [1]}
-                  
+                                 
